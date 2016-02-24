8dab5abebfa2890cd98421ab191b5c95515ef5d0
Joe Lloyd <josephglloyd@gmail.com>
2016-02-24 22:11:44 +0000

    Add noddy web server project

---
+++ NoddyWeb/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace NoddyWeb
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			const string baseUrl = "http://localhost:12345/";
+
+			using (WebApp.Start<Startup>(new StartOptions(baseUrl) { ServerFactory = "Microsoft.Owin.Host.HttpListener" }))
+			{
+				Console.WriteLine("Started, Press Return key to stop.");
+				Console.ReadLine();
+				Console.WriteLine("Stopped");
+			}
+		}
+	}
+}
---
+++ NoddyWeb/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace NoddyWeb
+{
+    public class Startup
+    {
+	    public void Configuration(IAppBuilder app)
+	    {
+		    app.Run(Invoke);
+	    }
+
+	    public Task Invoke(IOwinContext context)
+	    {
+		    context.Response.ContentType = "text/plain";
+		    return context.Response.WriteAsync(DateTime.Now.ToString(CultureInfo.CurrentCulture));
+	    }
+    }
+}
